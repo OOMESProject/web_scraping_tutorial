0e29c40b1e234d4683b66477f3e3ff103f604d94
jangkiru@gmail.com <jangkiru@gmail.com>
2025-07-07 15:20:58 +0900

    find, find_all 사용법 설명

--- src/test.py
+++ src/test.py
@@ -16,7 +16,7 @@ else:
 
 #3.HTML 파싱
 '''
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')  # 네이버의 html 문서가 다 나옴
 #html문서내용 출력됨
 print(soup)
 '''
@@ -36,6 +36,34 @@ html =""""
   </li>
  </ul>
 </nav>
+<nav id="hello">어서와</nav>
 """
 
 soup = BeautifulSoup(html, 'html.parser')
+
+
+print("== find 사용법 ==")
+#html 상에 있는 nav를 검색
+#그 중에 첫번째 nav를 검색
+print(soup.find('nav'))
+
+#html 상에 첫 번째 a 엘리먼트를 검색
+print(soup.find('a'))
+
+#html 상에 a를 검색하는데, class 이름이 google인 대상을 검색한다.
+print(soup.find('a', class_="google"))
+
+#html 상에 nav를 검색하는데 id가 hello인 대상을 검색한다.
+print(soup.find('nav', id="hello"))
+
+print("== find_all ==")
+
+print(soup.find_all('a'))
+
+a_el = soup.find_all('a')
+
+for idx, a in enumerate(a_el):
+  #  print(f"{idx} : {a}") 
+  #  print(f"{idx} : {a.get_text()}")  # a.get_text() : 엘리먼트를 제외한 텍스트만 추출
+  #  print(f"{idx} : {a.get('href')}")
+    print(f"{idx} : {a.get('class')}")
